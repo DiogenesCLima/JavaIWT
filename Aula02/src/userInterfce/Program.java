556b8ebe6d5afd9c31f96cf3aff62c9737bc6e18
Diogenes Costa <dcl_diogenes@hotmail.com>
2019-04-24 21:50:44 -0300

    Final da Aula

---
+++ Aula02/src/userInterfce/Program.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package userInterfce;
+
+/**
+ * @author Diogenes
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ola Mundo");
+		string nome = "";
+		int valor = 0;
+		double decimal = 0.00;
+		double resultado = (double) valor + decimal;
+		boolean logico = false;
+		int [] valores = {0, 2, 4, 6, 8, 10, 12};
+		int [] diassemana = {0, 1, 2, 3, 4, 5, 6};
+	}
+
+}
